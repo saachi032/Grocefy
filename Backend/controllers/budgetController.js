6fcaa205ff95b37ba2cdac70886868f4f92de031
saachi032 <saachi3205@gmail.com>
2026-01-05 23:34:37 +0530

    backend DONE done

--- Backend/controllers/budgetController.js
+++ Backend/controllers/budgetController.js
@@ -1,48 +1,48 @@
-// const Budget = require('../models/budgetModel');
+const Budget = require('../models/budgetModel');
 
-// // Get or create budget for user
-// const getBudget = async (req, res) => {
-//   try {
-//     let budget = await Budget.findOne({ userId: req.user._id });
+// Get or create budget for user
+const getBudget = async (req, res) => {
+  try {
+    let budget = await Budget.findOne({ userId: req.user._id });
 
-//     if (!budget) {
-//       // Create a default budget if none exists
-//       budget = await Budget.create({
-//         userId: req.user._id,
-//         monthlyBudget: 0,
-//       });
-//     }
+    if (!budget) {
+      // Create a default budget if none exists
+      budget = await Budget.create({
+        userId: req.user._id,
+        monthlyBudget: 0,
+      });
+    }
 
-//     res.json({ success: true, budget });
-//   } catch (error) {
-//     res.status(400).json({ success: false, message: error.message });
-//   }
-// };
+    res.json({ success: true, budget });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
 
-// // Update budget for user
-// const updateBudget = async (req, res) => {
-//   try {
-//     const { monthlyBudget } = req.body;
+// Update budget for user
+const updateBudget = async (req, res) => {
+  try {
+    const { monthlyBudget } = req.body;
 
-//     let budget = await Budget.findOne({ userId: req.user._id });
+    let budget = await Budget.findOne({ userId: req.user._id });
 
-//     if (!budget) {
-//       budget = await Budget.create({
-//         userId: req.user._id,
-//         monthlyBudget: monthlyBudget || 0,
-//       });
-//     } else {
-//       if (monthlyBudget !== undefined) budget.monthlyBudget = monthlyBudget;
-//       await budget.save();
-//     }
+    if (!budget) {
+      budget = await Budget.create({
+        userId: req.user._id,
+        monthlyBudget: monthlyBudget || 0,
+      });
+    } else {
+      if (monthlyBudget !== undefined) budget.monthlyBudget = monthlyBudget;
+      await budget.save();
+    }
 
-//     res.json({ success: true, budget });
-//   } catch (error) {
-//     res.status(400).json({ success: false, message: error.message });
-//   }
-// };
+    res.json({ success: true, budget });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
 
-// module.exports = {
-//   getBudget,
-//   updateBudget,
-// };
+module.exports = {
+  getBudget,
+  updateBudget,
+};
